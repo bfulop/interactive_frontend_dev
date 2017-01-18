4128e7b937949444d8aa62c7c398176b9cf4891d
Balint Fulop <bfulop@mac.com>
2017-01-18 17:14:33 +0100

    Added Aphrodite for scoped CSS

--- src/components/title.js
+++ src/components/title.js
@@ -1,9 +1,16 @@
 import Inferno from 'inferno'
+import { StyleSheet, css } from 'aphrodite'
+
+const styles = StyleSheet.create({
+  red: {
+    backgroundColor: 'orange'
+  }
+})
 
 function title (dispatch, {texts, children}) {
   return (
     <div>
-      <h1>{texts.text}</h1>
+      <h1 className={css(styles.red)}>{texts.text}</h1>
     </div>
   )
 }
