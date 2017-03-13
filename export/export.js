324847708d7dbbfb6595d0809a1c4e1791b55869
Balint Fulop <bfulop@mac.com>
2017-03-13 10:22:30 +0100

    Simple write to file of the component
    #2

--- export/export.js
+++ export/export.js
@@ -2,14 +2,8 @@ var Inferno = require('inferno')
 var InfernoServer = require('inferno-server')
 var createElement = require('inferno-create-element')
 var StyleSheetServer = require('aphrodite').StyleSheetServer
-
-var TestComponent = function () {
-  return createElement(
-    'div',
-    null,
-    'hello world'
-  )
-}
+var resolve = require('path').resolve
+var fs = require('fs')
 
 var FestivalsHeader = require('../src/components/FestivalsHeader').component
 
@@ -20,4 +14,23 @@ var builtComponent = StyleSheetServer.renderStatic(function () {
 builtComponent.css.content
 builtComponent.html
 
+function wrapHTML (html, css, js) {
+  return `
+    <html>
+        <head>
+            <style data-aphrodite>${css.content}</style>
+        </head>
+        <body>
+            <div id='root'>${html}</div>
+        </body>
+    </html>
+`
+}
+
 console.log(builtComponent)
+
+fs.writeFile(resolve('./export/export.html'), wrapHTML(builtComponent.html, builtComponent.css), (err) => {
+  if (err) throw err
+  console.log('It\'s saved!')
+})
+
